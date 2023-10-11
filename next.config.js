665f326037b4b5e8e83dfd5f86a50266ce1789d4
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-11 19:49:37 +0900

    add config files

--- next.config.js
+++ next.config.js
@@ -1,4 +1,27 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+/** @type {import('next').NextConfig} */
+
+const withPlugins = require("next-compose-plugins");
+const withBundleAnalyzer = require("@next/bundle-analyzer");
+
+const bundleAnalyzer = withBundleAnalyzer({
+	enabled: process.env.ANALYZE === "true",
+});
+
+const nextConfig = {
+	reactStrictMode: true,
+	distDir: ".next",
+	eslint: {
+		dirs: ["hooks", "models", "pages", "utils"], // Only run ESLint on the 'pages' and 'utils' directories during production builds (next build)
+	},
+	compiler: {
+		// Enables the styled-components SWC transform
+		styledComponents: true,
+	},
+};
+
+module.exports = withPlugins([[bundleAnalyzer]], nextConfig);
