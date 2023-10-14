6f099bcdf68635c85aa775921971d6c19e00903a
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-14 19:41:46 +0900

    create use me fetcher

---
+++ hooks/fetchers/user/useMe.ts
@@ -0,0 +1,21 @@
+import { BASE_API_URL } from "@/libs/constants";
+import { BACKEND_ROUTES } from "@/libs/routes";
+import { SWRFetcher } from "@/libs/utils";
+import useSWR from "swr";
+import { User } from "types";
+
+export const useMe = () => {
+	const apiUrl = `${BASE_API_URL}/${BACKEND_ROUTES.ME}`;
+
+	const {
+		data: user,
+		error,
+		isLoading,
+	} = useSWR(apiUrl, SWRFetcher<User>, {
+		revalidateIfStale: true,
+		revalidateOnFocus: false,
+		revalidateOnReconnect: false,
+	});
+
+	return { user, isLoading };
+};
