4a590de9c067379504fb08d38cfa990bd499aa69
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-14 19:35:01 +0900

    create guest wrapper components

---
+++ components/organisms/Wrapper/GuestFormWrapper.tsx
@@ -0,0 +1,71 @@
+import { Box, H3, LinkRouter, SParagraph } from "@/components/atoms";
+import { Divibar, ModalLoading } from "@/components/molecules";
+import { FC, ReactNode } from "react";
+import { shadows } from "@/styles";
+
+interface LinkProps {
+	href: string;
+	text: string;
+}
+interface Props {
+	title: string;
+	descriptions?: string[];
+	children: ReactNode;
+	footerLinks?: LinkProps[];
+	divibarText?: string;
+	isFormLoading: boolean;
+}
+
+export const GuestFormWrapper: FC<Props> = (props) => {
+	const {
+		title,
+		descriptions,
+		children,
+		footerLinks,
+		divibarText,
+		isFormLoading,
+	} = props;
+
+	return (
+		<>
+			<Box
+				backgroundColor="gray.white"
+				borderRadius="5px"
+				py="50px"
+				px="72px"
+				width="100%"
+				mx="auto"
+				maxWidth="550px"
+				style={{ boxShadow: shadows[20] }}
+			>
+				<Box mx="auto" textAlign="center">
+					<Box mb="20px">
+						<H3 textAlign="center" mb="20px" fontWeight={600}>
+							{title}
+						</H3>
+						{descriptions?.map((item, index) => {
+							return (
+								<SParagraph color="gray.500" key={index}>
+									{item}
+								</SParagraph>
+							);
+						})}
+					</Box>
+					{children}
+					{footerLinks && (
+						<>{divibarText ? <Divibar>{divibarText}</Divibar> : <Divibar />}</>
+					)}
+					{footerLinks?.map((item, index) => {
+						return (
+							<Box mb={1} key={index}>
+								<LinkRouter href={item.href} fontSize="14px">
+									{item.text}
+								</LinkRouter>
+							</Box>
+						);
+					})}
+				</Box>
+			</Box>
+		</>
+	);
+};
---
+++ components/organisms/Wrapper/index.ts
@@ -0,0 +1,4 @@
+export * from "./GuestFormWrapper"
+export * from "./GuestCardWrapper";
+export * from "./DashboardCardWrapper";
+export * from "./DashboardFormWrapper";
