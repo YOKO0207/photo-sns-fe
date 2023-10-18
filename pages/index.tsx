4b2d384f2d32264c3e4112d0651cb4117641f59a
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-18 18:50:56 +0900

    create posts page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,39 @@
-import { AppLayout } from "@/components/layouts"
-import { NextPage } from "next"
+import { AppLayout } from "@/components/layouts";
+import { NextPage } from "next";
 import { useUserContext } from "@/states/contexts";
+import { useMe, usePostIndexSWR } from "@/hooks";
+import { Image, Box } from "@/components/atoms";
+import { Posts } from "@/types";
+import { PostIndexCard } from "@/components/organisms";
+import styled from "styled-components";
+import { deviceSize } from "@/styles";
 
 const IndexPage: NextPage = () => {
-	const { state: user } = useUserContext();
+	const { data: posts, isLoading } = usePostIndexSWR();
 	return (
 		<AppLayout>
-			<div>Index Page</div>
+			<StyledBox>
+				{posts?.data?.data?.map((item: Posts, index: number) => (
+					<Box key={item.id} style={{ breakInside: "avoid" }}>
+						<PostIndexCard {...item} key={item.id} />
+					</Box>
+				))}
+			</StyledBox>
 		</AppLayout>
-	)
-}
+	);
+};
 
-export default IndexPage
+export default IndexPage;
+
+const StyledBox = styled(Box)`
+	column-count: 2;
+	@media only screen and (min-width: ${deviceSize.sm}px) {
+		column-count: 3;
+	}
+	@media only screen and (min-width: ${deviceSize.md}px) {
+		column-count: 4;
+	}
+	@media only screen and (min-width: ${deviceSize.lg}px) {
+		column-count: 5;
+	}
+`;
