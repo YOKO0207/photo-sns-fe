929107ae07f3ccbcbda048ce64773c515dc7d0c9
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-18 18:51:29 +0900

    modify styles

--- styles/globalStyles.ts
+++ styles/globalStyles.ts
@@ -6,10 +6,9 @@ export const GlobalStyles = createGlobalStyle`
     padding: 0;
     margin: 0;
     font-size: 16px;
-    background: ${({ theme }: any) => theme.colors[50]};
+    background: ${({ theme }: any) => theme.colors.gray.white};
     color: ${({ theme }: any) => theme.colors.gray["black"]};
-    font-family: Inter, Roboto, -apple-system, BlinkMacSystemFont, Segoe UI,
-    Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+    font-family: -apple-system, "system-ui", "Segoe UI", Roboto, Oxygen-Sans, Ubuntu, Cantarell, "Fira Sans", "Droid Sans", "Helvetica Neue", Helvetica, "ヒラギノ角ゴ Pro W3", "Hiragino Kaku Gothic Pro", メイリオ, Meiryo, "ＭＳ Ｐゴシック", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"
     sans-serif;
     line-height: 1.5;
     overflow-x: hidden;
@@ -36,15 +35,26 @@ export const GlobalStyles = createGlobalStyle`
     font-weight: regular;
   }
 
-	.file-drag-and-drop {
+.file-drag-and-drop {
 		width: 100%;
-		max-width: unset;
-		min-height: 200px;
-		background: white;
-		padding: 20px 40px;
-		border-color: ${({ theme }: any) => theme.colors.gray[300]};
+		padding-bottom: 85% !important;
+		min-width: unset !important;
+		background: white !important;
+		position: relative !important;
+		border-color: ${({ theme }: any) => theme.colors.gray[200]} !important;
+		svg {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			path {
+				fill: ${({ theme }: any) => theme.colors.gray[200]};
+			}
+		}
 		div {
 			flex-direction: column;
+			justify-content: center;
+			align-items: center;
 			span {
 				margin-bottom: 2px;
 			}
