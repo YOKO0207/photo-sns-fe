0c634aa0d4bf02396fe4bb78f48a96b4589f729d
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-19 20:37:39 +0900

    modify post fetcher

--- hooks/fetchers/index.ts
+++ hooks/fetchers/index.ts
@@ -1,2 +1,2 @@
 export * from "./user";
-export * from "./usePostFetcher";
+export * from "./post";
---
+++ hooks/fetchers/post/index.ts
@@ -0,0 +1,3 @@
+export * from "./usePostFetcher";
+export * from "./usePostThreadCommentFetcher";
+export * from "./usePostThreadFetcher";
