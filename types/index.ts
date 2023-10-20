f6ea6ff755f437ac6f336a4dc9a814c405097dbc
Yoko Yoshikawa <yokochan.0207@gmail.com>
2023-10-20 17:22:25 +0900

    create types

--- types/index.ts
+++ types/index.ts
@@ -2,3 +2,5 @@ export * from "./user";
 export * from "./util";
 export * from "./enum";
 export * from "./post";
+export * from "./postThread";
+export * from "./postThreadComment";
--- types/post.ts
+++ types/post.ts
@@ -2,15 +2,24 @@ import { ListUser } from "@/types";
 
 export type PostCreateInput = {
 	src: File | null;
-	post_name: string;
-	post_content: string;
+	post_name?: string;
+	post_content?: string;
 };
 
 export interface Posts {
 	id: number;
 	created_at: string;
 	src: string;
-	post_name: string;
-	post_content: string;
+	post_name?: string;
+	post_content?: string;
+	user: ListUser;
+}
+
+export interface Post {
+	id: number;
+	created_at: string;
+	src: string;
+	post_name?: string;
+	post_content?: string;
 	user: ListUser;
 }
